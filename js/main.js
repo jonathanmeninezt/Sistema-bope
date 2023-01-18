8bff60002946cf83bf50ecbb086058d91c822af7
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-01-17 22:11:53 -0300

    criação- form/information - function/click JS

--- js/main.js
+++ js/main.js
@@ -1,51 +1,47 @@
-// **************DOM******************
-
-// var h3 = document.getElementsByTagName('h3');
-
-// h3[2].innerHTML = 'Hackeado';
-
-// for(var i = 0; i < h3.length; i++){
-//     h3[i].innerHTML = 'Hackeado! -' ;
-// }
-
-
 var openRegister = document.getElementById("openWorld");
 var openRegister2 = document.getElementById("openMap");
+var openRegister3 = document.getElementById("openInformation");
+
+
 
 openRegister.addEventListener("click",()=>{
 
     var registro = document.getElementById("world");
-    
+   
 
     if(registro.style.display === "none"){
-        registro.style.display = "block";
+        registro.style.display = "block"
+     
     }else{
-        registro.style.display = "none";
-            
-        }
+        registro.style.display = "none";    
+    }
 });   
+
+
 
 openRegister2.addEventListener("click",()=>{
 
     var registro2 = document.getElementById("map");
-    var registro = document.getElementById("world");
-
-    if(registro.style.display === "none" && registro2.style.display ==="none"){
-        registro2.style.display = "block"; 
-    }else if
-    (registro.style.display === "block" && registro2.style.display ==="block"){
-        registro2.style.display = "none"; 
-       
+    
+    if(registro2.style.display === "none"){
+        registro2.style.display = "block";
     }else{
         registro2.style.display = "none";
-    
-    } 
+    }
+
 });
 
+openRegister3.addEventListener("click",()=>{
 
+    var registro3 = document.getElementById("information");
+    
+    if(registro3.style.display === "none"){
+        registro3.style.display = "block";
+    }else{
+        registro3.style.display = "none";
+    }
 
-
-
+});
 
 
 
