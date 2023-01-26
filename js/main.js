8e66094781fa3e1f24cd188b55d43ec8d7b22748
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-01-25 22:10:27 -0300

    criação- validação - input/button- cadastro pessoas

--- js/main.js
+++ js/main.js
@@ -7,10 +7,11 @@ var openRegister3 = document.getElementById("openInformation");
 openRegister.addEventListener("click",()=>{
 
     var registro = document.getElementById("world");
+  
    
 
     if(registro.style.display === "none"){
-        registro.style.display = "block"
+        registro.style.display = "block";
      
     }else{
         registro.style.display = "none";    
@@ -45,6 +46,22 @@ openRegister3.addEventListener("click",()=>{
 
 
 
+
+// ****************js-cadastros**************
+
+var cadastro = [];
+
+document.querySelector('button.cadastro').addEventListener('click',()=>{
+    var nomePessoa = document.querySelector('input[name=name_pessoa]').value;
+    var dataNascimento = document.querySelector('input[name=date_dn]').value;
+
+   cadastro.push({
+    nome: nomePessoa,
+    data: dataNascimento
+   })
+
+   alert(cadastro[0].nome);
+})
 
 
 
