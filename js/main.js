faf8548bd8f6c972ccf84ed263453d96383ff42e
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-01-27 20:06:01 -0300

    adicionando conteúdo- teste - div/list/new - cadastro

--- js/main.js
+++ js/main.js
@@ -54,31 +54,27 @@ var cadastro = [];
 document.querySelector('button.cadastro').addEventListener('click',()=>{
     var nomePessoa = document.querySelector('input[name=name_pessoa]');
     var dataNascimento = document.querySelector('input[name=date_dn]');
-
+    
    cadastro.push({
     nome: nomePessoa.value,
     data: dataNascimento.value
-   })
+   });
 
-   let dadosPessoas = document.querySelector('.list-box');
+   let dadosPessoas = document.querySelector('list-box');
    dadosPessoas.innerHTML = "";
 
    cadastro.map(function(val){
-    dadosPessoas.innerHTML+=`
-    <tr>
-    <td>`+val.nome+`</td>
-    <td>`+val.data+`</td>
-    <td>31/08/1990</td>
-    <td>21.747.046-7</td>
-    <td>(47)99705-2643</td>
-    <td>jonathan.meninezt@gmail.com</td>
-    <td><i class="fa-solid fa-pencil"></i></td>
-  </tr>
+    dadosPessoas.innerHTML+= `
+    <div class="list-single">
+     <h3>`+val.nome+`</h3>
+     <h3 class="date-new">`+val.data+`</h3>
+    </div><!--list-single-->
     `;
+   
+   
+    
    })
 
-   nomePessoa.value = "";
-   dataNascimento.value = "";
 });
 
 
